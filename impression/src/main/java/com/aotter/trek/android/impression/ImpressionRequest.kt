735028b29ec07b5f020b73ab960f7e2f50e6df11
Anthony <a173200488@gmail.com>
2021-05-07 16:12:19 +0800

    refactor: ImpressionRequest

--- impression/src/main/java/com/aotter/trek/android/impression/ImpressionRequest.kt
+++ impression/src/main/java/com/aotter/trek/android/impression/ImpressionRequest.kt
@@ -6,7 +6,7 @@ import androidx.annotation.IntRange
  * 2020/06/08 created by Anthony Wu
  * 曝光參數提供者
  * 該類提供開發者設置客製化的顯示百分比與停留時間
- * 可以針對 visibleRangePercent 與 dwellSeconds  預設為 50% 與 1秒
+ * 可以針對 visibleRangePercent 與 dwellSeconds  預設為 1% 與 1秒
  * 兩個參數決定 view 要顯示多少百分比在屏幕上才開始計算曝光，曝光的成立可由
  * dwellSeconds參數決定計算時長
  */
@@ -21,11 +21,13 @@ class ImpressionRequest {
     /**
      * view顯示多少百分比後才算是在可見範圍
      * @param percent 該參數決定顯示該view多少百分比
-     * 範圍限定是50％-100％
+     * 範圍限定是1％-100％
      */
-    fun setVisibleRangePercent(@IntRange(from = 50, to = 100) percent: Int): ImpressionRequest {
+    fun setVisibleRangePercent(@IntRange(from = 1, to = 100) percent: Int?): ImpressionRequest {
 
-        visibleRangePercent = percent
+        if (percent != null && percent > 0) {
+            visibleRangePercent = percent
+        }
 
         return this
     }
@@ -34,9 +36,11 @@ class ImpressionRequest {
      * view在可見範圍停留的秒數
      * @param millisInFuture 停留的秒數
      */
-    fun millisInFuture(millisInFuture: Long): ImpressionRequest {
+    fun millisInFuture(millisInFuture: Long?): ImpressionRequest {
 
-        this.millisInFuture = millisInFuture
+        if (millisInFuture != null && millisInFuture > 1000L) {
+            this.millisInFuture = millisInFuture
+        }
 
         return this
     }
